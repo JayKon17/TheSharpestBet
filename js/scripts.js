647cd2c043baaffbe617cbf7e56b054a60d0b9b9
JayKon17 <jonwrigley@gmail.com>
2025-02-28 05:50:05 -0500

    Shot Data Table Loads Properly

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
 
     const teamAbbreviations = {"Ottawa Senators":"OTT","Winnipeg Jets":"WPG","Colorado Avalanche":"COL","New Jersey Devils":"NJ","Los Angeles Kings":"LA","Vancouver Canucks":"VAN","Toronto Maple Leafs":"TOR","Montréal Canadiens":"MTL","Calgary Flames":"CGY","Edmonton Oilers":"EDM","Boston Bruins":"BOS","Buffalo Sabres":"BUF","Detroit Red Wings":"DET","Florida Panthers":"FLA","Chicago Blackhawks":"CHI","Minnesota Wild":"MIN","Carolina Hurricanes":"CAR","Dallas Stars":"DAL","Anaheim Ducks":"ANA","Columbus Blue Jackets":"CBJ","Philadelphia Flyers":"PHI","Nashville Predators":"NSH","St Louis Blues":"STL","Pittsburgh Penguins":"PIT","Arizona Coyotes":"ARI","Tampa Bay Lightning":"TB","San Jose Sharks":"SJ","Washington Capitals":"WSH","New York Rangers":"NYR","New York Islanders":"NYI","Seattle Kraken":"SEA","Vegas Golden Knights":"VGK","Utah Hockey Club":"UTAH","Arizona Cardinals":"ARI","Atlanta Falcons":"ATL","Baltimore Ravens":"BAL","Buffalo Bills":"BUF","Carolina Panthers":"CAR","Chicago Bears":"CHI","Cincinnati Bengals":"CIN","Cleveland Browns":"CLE","Dallas Cowboys":"DAL","Denver Broncos":"DEN","Detroit Lions":"DET","Green Bay Packers":"GB","Houston Texans":"HOU","Indianapolis Colts":"IND","Jacksonville Jaguars":"JAX","Kansas City Chiefs":"KC","Las Vegas Raiders":"LV","Los Angeles Chargers":"LAC","Los Angeles Rams":"LAR","Miami Dolphins":"MIA","Minnesota Vikings":"MIN","New England Patriots":"NE","New Orleans Saints":"NO","New York Giants":"NYG","New York Jets":"NYJ","Philadelphia Eagles":"PHI","Pittsburgh Steelers":"PIT","San Francisco 49ers":"SF","Seattle Seahawks":"SEA","Tampa Bay Buccaneers":"TB","Tennessee Titans":"TEN","Washington Football Team":"WAS","Atlanta Hawks":"ATL","Boston Celtics":"BOS","Brooklyn Nets":"BKN","Charlotte Hornets":"CHA","Chicago Bulls":"CHI","Cleveland Cavaliers":"CLE","Dallas Mavericks":"DAL","Denver Nuggets":"DEN","Detroit Pistons":"DET","Golden State Warriors":"GSW","Houston Rockets":"HOU","Indiana Pacers":"IND","Los Angeles Clippers":"LAC","Los Angeles Lakers":"LAL","Memphis Grizzlies":"MEM","Miami Heat":"MIA","Milwaukee Bucks":"MIL","Minnesota Timberwolves":"MIN","New Orleans Pelicans":"NO","New York Knicks":"NYK","Oklahoma City Thunder":"OKC","Orlando Magic":"ORL","Philadelphia 76ers":"PHI","Phoenix Suns":"PHX","Portland Trail Blazers":"POR","Sacramento Kings":"SAC","San Antonio Spurs":"SAS","Toronto Raptors":"TOR","Utah Jazz":"UTAH","Washington Wizards":"WAS","Arizona Diamondbacks":"ARI","Atlanta Braves":"ATL","Baltimore Orioles":"BAL","Boston Red Sox":"BOS","Chicago Cubs":"CHC","Chicago White Sox":"CWS","Cincinnati Reds":"CIN","Cleveland Indians":"CLE","Colorado Rockies":"COL","Detroit Tigers":"DET","Houston Astros":"HOU","Kansas City Royals":"KC","Los Angeles Angels":"LAA","Los Angeles Dodgers":"LAD","Miami Marlins":"MIA","Milwaukee Brewers":"MIL","Minnesota Twins":"MIN","New York Mets":"NYM","New York Yankees":"NYY","Oakland Athletics":"OAK","Philadelphia Phillies":"PHI","Pittsburgh Pirates":"PIT","San Diego Padres":"SD","San Francisco Giants":"SF","Seattle Mariners":"SEA","St. Louis Cardinals":"STL","Tampa Bay Rays":"TB","Texas Rangers":"TEX","Toronto Blue Jays":"TOR","Washington Nationals":"WSH"};
-    const API_key = "5be9118d712dcce89e543dbfa3a1de27"
+    const API_key = "5be9118d712dcce89e543dbfa3a1de2" // 7 is last digit
 
     // Data structure to manage sport-related content
     const sportsData = {
@@ -78,6 +78,7 @@ document.addEventListener("DOMContentLoaded", () => {
             <div id="suggestions-container" class="suggestions-container"></div>
         `;
         document.getElementById("live-scores-container").style.display = "block";
+        document.getElementById("shot-analysis-container").style.display = "block";
         fetchLiveNHLScore();  // Fetch live NHL scores
     }
 
@@ -406,13 +407,9 @@ document.addEventListener("DOMContentLoaded", () => {
             // Percentage
             percentages.over[index] = ((overCount / gameLogData.length) * 100).toFixed(2) + "%";
             percentages.under[index] = ((underCount / gameLogData.length) * 100).toFixed(2) + "%";
-    
-            // Breakeven (just as an example calculation, adjust as needed)
-            breakevens.over[index] = (threshold / overCount).toFixed(2);
-            breakevens.under[index] = (threshold / underCount).toFixed(2);
         });
     
-        return { counts, percentages, breakevens };
+        return { counts, percentages };
     }
     
 
@@ -569,9 +566,6 @@ document.addEventListener("DOMContentLoaded", () => {
                     day: '2-digit',
                 }); // e.g., "02/27" or ""
 
-                console.log(formattedDate);
-
-                
                 const formattedTime = gameDate.toLocaleTimeString('en-US', {
                     hour: 'numeric',
                     minute: '2-digit',
